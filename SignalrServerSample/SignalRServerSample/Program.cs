c9bac6c07b0a07bacb8cd3e8827451a0c79162da
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3.co.at>
2014-01-13 11:19:21 +0100

    added signalr server sample

---
+++ SignalrServerSample/SignalRServerSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRServerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://192.168.1.137:8080"))
+            {
+                Console.WriteLine("Server running at http://192.168.1.137:8080/");
+                Console.ReadLine();
+            }
+        }
+    }
+}
